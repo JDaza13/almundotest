41e102c5ba63316b1e80764f87004bf245b05445
jdaza13 <jdaza1327@gmail.com>
2018-08-16 04:20:01 +0000

    Removed unused resources

--- almundotest/src/app/hotel.service.ts
+++ almundotest/src/app/hotel.service.ts
@@ -3,12 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
-import { MessageService } from './message.service';
 import { Hotel } from './hotel';
-
-import { HOTELS } from './mock-hotels';
-
-
 
 @Injectable({
   providedIn: 'root'
@@ -18,15 +13,9 @@ export class HotelService {
   private hotelsUrl = 'https://almundotest-jdaza13.c9users.io:8081/hotels'; 
 
   constructor(
-    private http: HttpClient,
-    private messageService: MessageService) { }
-    
-  private log(message: string) {
-    this.messageService.add(`HotelService: ${message}`);
-  }
-  
+    private http: HttpClient) { }
+
   getHotels(): Observable<Hotel[]> {
-    this.messageService.add('HotelService: fetched hotels');
     return this.http.get<Hotel[]>(this.hotelsUrl)
   }
   
